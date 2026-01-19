ee4d646189f6ab3f02735f8f471625097b8c64ed
lizz <lizazinha2.0@gmail.com>
2026-01-19 08:58:19 -0300

    adjustment to the layout and application of the password viewing functionality

--- scripts.js
+++ scripts.js
@@ -10,4 +10,32 @@ let container = document.getElementById('main-container');
         container.classList.remove("active");
     });
 
+
+let inputsSenha = document.querySelectorAll('.password');
+inputsSenha.forEach(input => {
+    input.addEventListener('mousemove', function(e) {
+        if (e.offsetX > (this.offsetWidth - 35)) {
+            this.style.cursor = 'pointer';
+        } else {
+            this.style.cursor = 'text';
+        }
+    });
+
     
+    input.addEventListener('mousedown', function(e) {
+        let larguraTotal = this.offsetWidth;
+        let cliqueX = e.offsetX;
+
+        if (cliqueX > (larguraTotal - 35)) {
+            e.preventDefault(); 
+
+            if (this.type === 'password') {
+                this.type = 'text';
+                this.classList.add('show');
+            } else {
+                this.type = 'password';
+                this.classList.remove('show');
+            }
+        }
+    });
+});
